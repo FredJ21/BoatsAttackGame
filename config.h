afe933e3a7d5edf5356589fbe79a2577e0b52c57
FredJ21 <frederic@jelmoni.fr>
2015-10-23 19:37:20 +0200

    save level Mathis

--- config.h
+++ config.h
@@ -27,7 +27,7 @@
 #define TILE_TAILLE_Y         32
 #define TILE_FILE           "./images/32x32_map_tile_v3.1.bmp"
 #define TILE_FILE_NB_COLONNE  23
-#define LEVEL_NB_TOTAL            6
+#define LEVEL_NB_TOTAL            4
 #define WAVE_ENEMY_MAX_BY_WAVE  100
 #define WAVE_NB                  10
 #define TOWER_MAX                10
