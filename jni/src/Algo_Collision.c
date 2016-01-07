4eb27ce0939d8be3cdd87166dac424ac2a59bfdc
Fredj21 <frederic@jelmoni.fr>
2016-01-07 19:05:33 +0100

    modif appel fonction test_collision

--- jni/src/Algo_Collision.c
+++ jni/src/Algo_Collision.c
@@ -35,44 +35,45 @@ bool test_collision_circle      (int Ax, int Ay, int Aradius, int Bx, int By, in
 }
 /*****************************************************************
 *****************************************************************/
-void test_collision             (t_tower *tower[], int nb_tower, t_sprite *enemy[], int nb_enemy) {
+//void test_collision             (t_tower *tower[], int nb_tower, t_sprite *enemy[], int nb_enemy) {
+void test_collision             (t_game *my_game) {
 
     int a, b, m, s;
     int nb = 0;
 
-    for (a = 0; a < nb_tower; a++) {                        // pour chaque tourelle  A
+    for (a = 0; a < my_game->current_nb_tower; a++) {                        // pour chaque tourelle  A
         for( m = 0; m < TOWER_NB_MISSILE_MAX; m++){         // pour chaque missile   M
 
-            if (tower[a]->missile[m].actif ) {
+            if (my_game->sp_TOWER[a]->missile[m].actif ) {
 
                 // test de collision avec les autres tourelle   B
-                for (b = 0; b < nb_tower; b++) {
+                for (b = 0; b < my_game->current_nb_tower; b++) {
 
-                    if ( tower[b]->actif && a != b) {       // pas de test entre une tourelle et elle meme
+                    if ( my_game->sp_TOWER[b]->actif && a != b) {       // pas de test entre une tourelle et elle meme
 
                         nb++;
                                                             // test de collision entre le missile M de la tourelle A avec la position de tourelle B
-                        if ( test_collision_circle( tower[a]->missile[m].x, tower[a]->missile[m].y, 5, tower[b]->x, tower[b]->y, tower[b]->anim->tx/2 ) ) {
+                        if ( test_collision_circle( my_game->sp_TOWER[a]->missile[m].x, my_game->sp_TOWER[a]->missile[m].y, 5, my_game->sp_TOWER[b]->x, my_game->sp_TOWER[b]->y, my_game->sp_TOWER[b]->anim->tx/2 ) ) {
 
-                            tower[a]->missile[m].actif = false;
-                            tower[b]->actif = false;
+                            my_game->sp_TOWER[a]->missile[m].actif = false;
+                            my_game->sp_TOWER[b]->actif = false;
 
                         }
                     }
                 }
 
                 // test de collision avec les enemy
-                for ( s = 0; s < nb_enemy; s++ ){
+                for ( s = 0; s < my_game->current_nb_enemy; s++ ){
 
-                    if ( enemy[s]->is_actif ) {
+                    if ( my_game->sp_ENEMY[s]->is_actif ) {
 
                         nb++;
                                                             // test de collision entre le missile M de la tourelle A
-                        if ( test_collision_circle( tower[a]->missile[m].x, tower[a]->missile[m].y, 5, enemy[s]->x, enemy[s]->y, enemy[s]->anim->tx/2 )) {
+                        if ( test_collision_circle( my_game->sp_TOWER[a]->missile[m].x, my_game->sp_TOWER[a]->missile[m].y, 5, my_game->sp_ENEMY[s]->x, my_game->sp_ENEMY[s]->y, my_game->sp_ENEMY[s]->anim->tx/2 )) {
 
-                            if ( tower[a]->missile[m].actif ) {
-                                tower[a]->missile[m].actif = false;
-                                enemy[s]->is_actif = false;
+                            if ( my_game->sp_TOWER[a]->missile[m].actif ) {
+                                my_game->sp_TOWER[a]->missile[m].actif = false;
+                                my_game->sp_ENEMY[s]->is_actif = false;
                             }
 
                         }
--- jni/src/Algo_Collision.h
+++ jni/src/Algo_Collision.h
@@ -1,12 +1,12 @@
 #ifndef ALGO_COLLISION_H_INCLUDED
 #define ALGO_COLLISION_H_INCLUDED
 
-
+#include "game.h"
 
 
 bool test_collision_circle      (int Ax, int Ay, int Aradius, int Bx, int By, int Bradius );
-void test_collision             (t_tower *tower[], int nb_tower, t_sprite *enemy[], int nb_enemy);
 
+void test_collision             (t_game *my_game);
 
 
 #endif // ALGO_COLLISION_H_INCLUDED
