13e528bd6af4a823f564742de59bc9a0e6ad318b
FredJ21 <frederic@jelmoni.fr>
2016-01-16 12:46:24 +0100

    ADD SDL2_image sous Android/Eclipse

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -5,8 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := main
 
 SDL_PATH := ../SDL
-
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include
+
+
 
 # Add your application source files here...
 LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
@@ -20,7 +21,7 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.c \
 	menu.c
 
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf SDL2_image
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog
 
